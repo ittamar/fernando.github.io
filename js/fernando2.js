bc4490668bf3ea9031a175828b171a1c44f340f3
ittamar <itamargomes038@hotmail.com>
2016-07-19 11:05:15 -0300

    commit inicial

---
+++ js/fernando2.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+$('.button-collapse').sideNav(); /*esta função que  oculta o menu*/
+
+});
+
+ $(document).ready(function(){
+      $(".parallax").parallax();
+
+     });
+
+
+
