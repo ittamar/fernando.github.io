955fe73fb9c1d9826082d253ac141d8ed36768d1
ittamar <itamargomes038@hotmail.com>
2016-08-01 12:27:23 -0300

    autualização 01/08/2016

--- js/fernando2.js
+++ js/fernando2.js
@@ -8,5 +8,21 @@ $('.button-collapse').sideNav(); /*esta função que  oculta o menu*/
 
      });
 
+ $(document).ready(function(){
+      $('.slider').slider({full_width: true});
+    });
+
+/*$(document).ready(function(){
+    $("#sobre").hover(function(){
+window.alert("Profissional na área da construção civil a mais de vinte anos, atuando na grande joão pessoa e cidades vizinhas tendo trabalhado em empresas de grande porte na área da construção civil. ");
+
+    });
+});
+
+$(document).ready(function(){
+    $("#endereco").mouseup(function(){
+        window.alert("Rua Antônio ferreira da silva, 239  ");
+    });
+});*/
 
 
